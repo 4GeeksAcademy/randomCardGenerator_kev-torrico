7d908e79060f82de89752ef9176a4e0492129f61
Kevin Cristhian Villca Torrico <kevtorricov@gmail.com>
2024-09-24 00:10:33 +0000

    Completed Card Generator

--- src/app.js
+++ src/app.js
@@ -7,5 +7,42 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  function aleatorio(lista) {
+    let numeroAleatorio = Math.floor(Math.random() * lista.length);
+    return lista[numeroAleatorio];
+  }
+  let iconos = ["♦", "♥", "♠", "♣"];
+  let numeros = [
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+
+  let icono = aleatorio(iconos);
+  let elementIconoArriba = document.querySelector("#iconoArriba");
+  elementIconoArriba.innerHTML = icono;
+
+  let numeroCarta = aleatorio(numeros);
+  let elementNumCarta = document.getElementById("numeroCarta");
+  elementNumCarta.innerHTML = numeroCarta;
+
+  let elementIconoAbajo = document.querySelector("#iconoAbajo");
+  elementIconoAbajo.innerHTML = icono;
+
+  elementIconoAbajo.style.transform = "rotate(180deg)";
+
+  if (icono == "♥" || icono == "♦") {
+    elementIconoAbajo.style.color = "red";
+    elementIconoArriba.style.color = "red";
+  }
 };
